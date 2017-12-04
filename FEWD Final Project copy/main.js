95727b8005162fe7a12fa2914949bc506920f2ba
IanWFerguson <iwferguso25@gmail.com>
2017-12-04 16:34:07 -0700

    My Best Version

---
+++ FEWD Final Project copy/main.js
@@ -0,0 +1,7 @@
+$( document ).ready(function(){
+    jQuery(function(){
+            $( '.hamburger' ).click(function(){
+                $('nav').toggleClass('show')
+            })
+       })
+});
